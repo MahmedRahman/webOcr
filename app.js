620132a11cf7ffab4e9629c4e2a991848b404b85
atpfreelancer <atpfreelancer@gmail.com>
2024-05-02 13:01:03 +0300

    webocr

---
+++ app.js
@@ -0,0 +1,35 @@
+const apiUrl = "http://192.168.1.104:8000/api/upload";
+
+const outputElement = document.getElementById("output");
+const submitBtn = document.getElementById("submit-btn");
+
+submitBtn.onclick = function () {
+  myFunction();
+};
+
+function myFunction() {
+  const fileInput = document.getElementById("fileselect").files[0];
+
+  let formData = new FormData();
+  formData.append("image", fileInput); // Ensure you append the file, not the input element
+  formData.append("prompt", "total");
+
+  fetch(apiUrl, {
+    method: "POST",
+    body: formData, // Send formData directly without JSON.stringify
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Display data in an HTML element
+      outputElement.textContent = JSON.stringify(data, null, 2);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      outputElement.textContent = "Failed to process request.";
+    });
+}
